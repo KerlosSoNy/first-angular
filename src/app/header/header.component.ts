294d06ea179610ae2c69c76861168b56ee0a8be5
kero <kerlossony@yahoo.com>
2024-09-14 12:37:56 +0300

    maping useing *ngFor

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   title='Angular Directives';
+  data = [{name:'Zoniix', age: 28}, {name:'Ahmed', age: 10}, {name:'Mohamed', age: 23}];
 }
